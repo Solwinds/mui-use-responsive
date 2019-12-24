5208da7e64a72bad9c2f7a3b8bcb04f84ad360ae
Roy Xu <boyuanxu9@gmail.com>
2019-12-24 04:08:14 -0500

    implement source code

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 'use strict';
+import { useMediaQuery } from '@material-ui/core'
 
-module.exports = (input, {postfix = 'rainbows'} = {}) => {
-	if (typeof input !== 'string') {
-		throw new TypeError(`Expected a string, got ${typeof input}`);
-	}
+export default function useResponsive(designWidth = 1920, breakpoints = [600, 960, 1280, 1600, 1920,]) {
+  //const theme = useTheme()
 
-	return `${input} & ${postfix}`;
-};
+  const breakpoint = breakpoints.slice().reverse().reduce((output, width) => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const matches = useMediaQuery(`(min-width: ${width+1}px)`)
+    return !output && matches ? width : output
+  }, 0) || breakpoints[0]
+
+  return breakpoint / designWidth
+}
+
+export const responsive = (...values) => {
+  //eslint-disable-next-line fp/no-let
+  let unit = 'px'
+
+  if (isNaN(values[values.length - 1])) {
+    unit = values.pop()
+  }
+
+  return ({scale}) => {
+    return values.map(val => '' + val * scale + unit).join(' ')
+  }
+}
--- test.js
+++ test.js
@@ -1,13 +1,13 @@
 import test from 'ava';
-import unicornFun from '.';
+import useResponsive, {responsive} from '.';
 
 test('main', t => {
-	t.throws(() => {
-		unicornFun(123);
-	}, {
-		instanceOf: TypeError,
-		message: 'Expected a string, got number'
-	});
+	// t.throws(() => {
+	// 	unicornFun(123);
+	// }, {
+	// 	instanceOf: TypeError,
+	// 	message: 'Expected a string, got number'
+	// });
 
-	t.is(unicornFun('unicorns'), 'unicorns & rainbows');
+	// t.is(unicornFun('unicorns'), 'unicorns & rainbows');
 });
